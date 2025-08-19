724a86cefaf9271619f0202e2440ab818e635b6f
BrandonLewis <brandon.lewis@allianceinnovations.com>
2025-08-19 13:42:05 -0700

    do not track

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
         defer
         src="https://stg.platform.analytics.nexus/script.js"
         data-website-id="d9d0d65d-2307-41ed-9ca2-1fdb35133f6a"
+        data-do-not-track="true"
       />
       <body className={inter.className}>{children}</body>
     </html>
